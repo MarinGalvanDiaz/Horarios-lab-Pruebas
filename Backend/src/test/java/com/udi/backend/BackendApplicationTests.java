7fd9851d61f00c2ecc98f6ca47c51d43b26ecbb1
MarinGalvanDiaz <galvandiazmarin@gmail.com>
2026-05-18 16:29:40 -0600

    Primer commit

---
+++ Backend/src/test/java/com/udi/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
